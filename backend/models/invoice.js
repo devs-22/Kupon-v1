09b0a5eb20828ae2b0ece02ddc9aad928d79fabb
Uche Omodu <79480742+qgatssdev@users.noreply.github.com>
2022-02-23 14:04:38 +0100

    Delete backend directory

--- backend/models/invoice.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose")
-
-const invoiceSchema = new mongoose.Schema({
-    name: String
-  });
-
-const Invoice = mongoose.model('Invoice', invoiceSchema);
-
-module.exports = Invoice  
