df184d30afde014a100700bdce2e4f1dd8eb51ec
qgatssdev <chivoomodu@gmail.com>
2022-02-28 15:24:34 +0100

    finished footer and added font

---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import './Footer.css';
+import { ReactComponent as Twitter } from '../../assets/SVGs/Twitter.svg';
+import { ReactComponent as Instagram } from '../../assets/SVGs/instagram.svg';
+
+const Footer = () => {
+  const today = new Date();
+  return (
+    <div>
+      <div className="footer-container">
+        <div>
+          <button>FAQs</button>
+        </div>
+        <div>
+          <button>Terms</button>
+        </div>
+        <div>
+          <button>Privacy</button>
+        </div>
+      </div>
+      <div className="contact-button">
+        <button>Contact us</button>
+      </div>
+      <hr />
+      <p className="footer-year">@ {today.getFullYear()} Kupon Technology</p>
+      <h1>Kupon</h1>
+      <div className="socials">
+        <Instagram className="ig" />
+        <Twitter className="twitter" />
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
