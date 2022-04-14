277f5fac59a150b9f94f0ebefe36af8e9c5a5980
deji-ice <ayodejiatanda1@gmail.com>
2022-04-15 00:04:12 +0100

    resolved merge conflict

--- src/pages/GetToKnowUs/GetToKnowUs.tsx
+++ src/pages/GetToKnowUs/GetToKnowUs.tsx
@@ -2,66 +2,60 @@ import styled from "styled-components"
 import Footer from '../../components/Footer/Footer';
 import Header from '../../components/Header/Header';
 import Flower from '../../assets/IMAGES/orangeFLOWER.png';
+import Hand from '../../assets/IMAGES/Point_right.png';
 
 const GetToKnowUs = () => {
   return (
-    <>
-    <Header />
-    <Container>
+    <div>
+      <Header />
+      <Container>
         <LogoNTextsContainer>
-        {/* <Image src={Hand} alt="hand"/> */}
-        <Image src={Flower} alt="blue flower"/>
-            <H1>Get to know us</H1>
-        <Image src={Flower} alt="blue flower"/>
+          <HandImage src={Hand} alt="hand" />
+          <Image src={Flower} alt="blue flower" />
+          <H1>Get to know us</H1>
+          <Image src={Flower} alt="blue flower" />
         </LogoNTextsContainer>
         <TextContainer>
-        <P> <Span>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pretium sit in nisl dignissim purus.</Span> <Span> Mollis tellus pulvinar sit nisl lacus a velit sed orci. 
-            Sit feugiat eget id augue amet tincidunt. Urna suspttendisse nisl leo ultrices nam tincidunt et id a.</Span></P>
-          
+        <P> <Span>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pretium sit in nisl dignissim purus.</Span>
+            <Span> Mollis tellus pulvinar sit nisl lacus a velit sed orci. 
+            Sit feugiat eget id augue amet tincidunt. Urna suspttendisse nisl leo ultrices nam tincidunt et id a.</Span>
+        </P>
         </TextContainer>
-    </Container>
-    <Footer />
-    </>
-  )
-}
+      </Container>
+      <Footer />
+    </div>
+  );
+};
 
-export default GetToKnowUs
+export default GetToKnowUs;
 
 const Container = styled.div`
 display: flex;
 flex-direction: column;
 align-items: center;
 margin: 9rem 5.5rem;
-
 @media (max-width: 768px){
 margin: auto;
 }
 @media (min-width: 769px) and ( max-width: 992px){
   margin: 5rem;
 }
-`
+`;
 const LogoNTextsContainer = styled.div`
 display: flex;
 margin-bottom: 1.5rem;
 width: 80%;
 align-items: center;
 justify-content: center;
-
-
 @media (max-width: 768px){
 width: 70%;
 margin-bottom: 0.5rem;
-
 }
-
 @media (min-width: 769px) and ( max-width: 992px){
 width: 100%;
-
 }
-
-`
+`;
 const TextContainer = styled.div`
-
 display: flex;
 flex-direction: column;
 background-color: rgba(135, 190, 228, 0.1);
@@ -71,12 +65,10 @@ height: 29rem;
 
 @media (max-width: 768px){
 height: 100%;
-
 }
 
 @media (min-width: 769px) and ( max-width: 992px){
   height: 100%;
-
 }
 `
 const P = styled.p`
@@ -104,25 +96,46 @@ flex-direction: column;
 margin: 0;
 line-height: 3rem;
 }
+
+@media (min-width: 769px) and ( max-width: 992px){
+  display: flex;
+flex-direction: column;
+  font-size: 20px;
+margin: 0;
+line-height: 3rem;
+}
 `
 
 
 
 const Image = styled.img`
-height: 80px;
-width: 130px;
+  height: 80px;
+  width: 130px;
 
-@media (max-width: 768px){
-height: 20px;
-width: 35px;
-}
-`
+  @media (max-width: 768px) {
+    height: 20px;
+    width: 35px;
+  }
+`;
+
+const HandImage = styled.img`
+  position: absolute;
+  width: 100px;
+  height: 123.6px;
+  left: 0;
+
+  @media (max-width: 768px) {
+    width: 60px;
+    height: 60px;
+    left: -48px;
+  }
+`;
+
 const H1 = styled.h1`
 font-size: 58px;
 padding: 2rem;
 text-align: center;
 line-height: 4rem;
-
 @media (max-width: 768px){
 font-size: 33px;
 width: 55%;
